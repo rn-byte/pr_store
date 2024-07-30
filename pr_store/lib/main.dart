5371cfc4466694b129d65c34ac1f347574ac43ef
rn-byte <ramthakur977+github@gmail.com>
2024-07-30 14:30:15 +0545

    some deoendency and assets added

--- pr_store/lib/main.dart
+++ pr_store/lib/main.dart
@@ -6,16 +6,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      title: 'PR-Store',
+      theme: ThemeData(),
+      darkTheme: ThemeData(),
     );
   }
 }
