11910b08e329f21ccad02b1ec8eb5317fe9f72b6
rn-byte <ramthakur977+github@gmail.com>
2024-07-30 15:43:27 +0545

    light and dark theme added for appBar and BottomSheet

--- pr_store/lib/utils/theme/theme.dart
+++ pr_store/lib/utils/theme/theme.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:pr_store/utils/theme/custom_themes/appbar_theme.dart';
 import 'package:pr_store/utils/theme/custom_themes/elevated_button_theme.dart';
 import 'package:pr_store/utils/theme/custom_themes/text_theme.dart';
 
 class PrAppTheme {
   PrAppTheme._();
   static ThemeData lightTheme = ThemeData(
-    useMaterial3: true,
-    fontFamily: 'Poppins',
-    brightness: Brightness.light,
-    primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
-    textTheme: PrTextTheme.lightTextTheme,
-    elevatedButtonTheme: PrElevatedButtonTheme.lightElevatedButtonTheme,
-  );
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: PrTextTheme.lightTextTheme,
+      elevatedButtonTheme: PrElevatedButtonTheme.lightElevatedButtonTheme,
+      appBarTheme: PrAppBarTheme.lightAppBarTheme);
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
@@ -21,5 +22,6 @@ class PrAppTheme {
     scaffoldBackgroundColor: Colors.black,
     textTheme: PrTextTheme.darkTextTheme,
     elevatedButtonTheme: PrElevatedButtonTheme.darkElevatedButtonTheme,
+    appBarTheme: PrAppBarTheme.darkAppBarTheme,
   );
 }
