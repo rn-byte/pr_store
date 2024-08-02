00c77a0b3c9e9d677ddc3067927364b1bf308276
rn-byte <ramthakur977+github@gmail.com>
2024-08-02 14:28:35 +0545

    code refractoring done

--- pr_store/lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ pr_store/lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
-
-import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
-import '../../../../../utils/helpers/helper.dart';
 import 'terms_conditions.dart';
 
 class SignupForm extends StatelessWidget {
