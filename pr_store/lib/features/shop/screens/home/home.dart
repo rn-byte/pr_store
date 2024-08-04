4c87d58cc074a669f570becc80d30cdf9c929f5b
rn-byte <ramthakur977+github@gmail.com>
2024-08-04 22:03:47 +0545

    custom shape container added for home screen

--- pr_store/lib/features/shop/screens/home/home.dart
+++ pr_store/lib/features/shop/screens/home/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pr_store/utils/constants/colors.dart';
+
+import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,11 +9,22 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Home Screen'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: PrColor.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  PrCircularContainer(
+                    backgroundColor: PrColor.textWhite.withOpacity(0.1),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
