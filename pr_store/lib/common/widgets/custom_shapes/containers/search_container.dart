afa9b979869552c549744c772a850b4e436170b7
rn-byte <ramthakur977+github@gmail.com>
2024-08-05 23:58:48 +0545

    code refractor done

---
+++ pr_store/lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/devices/devices_utility.dart';
+import '../../../../utils/helpers/helper.dart';
+
+class PrSearchContainer extends StatelessWidget {
+  const PrSearchContainer({
+    super.key,
+    required this.text,
+    this.icon = Iconsax.search_normal,
+    this.showBackground = true,
+    this.showBorder = true,
+  });
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = PrHelper.isDarkMode(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: PrSizes.defaultSpace),
+      child: Container(
+        width: PrDeviceUtils.getScreenWidth(),
+        padding: const EdgeInsets.all(PrSizes.md),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(PrSizes.cardRadiusLg),
+          color: showBackground
+              ? isDark
+                  ? PrColor.dark
+                  : PrColor.light
+              : Colors.transparent,
+          border: showBorder ? Border.all(color: PrColor.grey) : null,
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: PrColor.darkGrey,
+            ),
+            const SizedBox(
+              width: PrSizes.spaceBtwItems,
+            ),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- pr_store/lib/features/shop/screens/home/home.dart
+++ pr_store/lib/features/shop/screens/home/home.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:pr_store/features/shop/screens/home/home_app_bar.dart';
-import 'package:pr_store/utils/constants/colors.dart';
 import 'package:pr_store/utils/constants/sizes.dart';
-import 'package:pr_store/utils/devices/devices_utility.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -19,42 +17,14 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                 children: [
                   ///AppBar
-                  const PrHomeAppBar(),
-                  const SizedBox(
+                  PrHomeAppBar(),
+                  SizedBox(
                     height: PrSizes.spaceBtwSections,
                   ),
 
                   ///Search Bar
 
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: PrSizes.defaultSpace),
-                    child: Container(
-                      width: PrDeviceUtils.getScreenWidth(),
-                      padding: const EdgeInsets.all(PrSizes.md),
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(PrSizes.cardRadiusLg),
-                        color: PrColor.white,
-                        border: Border.all(color: PrColor.grey),
-                      ),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Iconsax.search_normal,
-                            color: PrColor.darkGrey,
-                          ),
-                          const SizedBox(
-                            width: PrSizes.spaceBtwItems,
-                          ),
-                          Text(
-                            'Search in store',
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
+                  PrSearchContainer(text: 'Search in store'),
 
                   /// categories section
                 ],
