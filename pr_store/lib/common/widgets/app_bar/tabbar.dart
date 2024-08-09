824ae33b83540c8c8bf937902419bb05f4bd529f
rn-byte <ramthakur977+github@gmail.com>
2024-08-09 21:03:05 +0545

    TabBar added

---
+++ pr_store/lib/common/widgets/app_bar/tabbar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:pr_store/utils/constants/colors.dart';
+import 'package:pr_store/utils/devices/devices_utility.dart';
+import 'package:pr_store/utils/helpers/helper.dart';
+
+class PrTabBar extends StatelessWidget implements PreferredSizeWidget {
+  ///if you want to add background color to  TAB, you have to wrap them in material Widget.
+  ///To do that we need [PreferredSize] widget and that's why we created custom class.
+  const PrTabBar({super.key, required this.tabs});
+
+  final List<Widget> tabs;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = PrHelper.isDarkMode(context);
+    return Material(
+      color: isDark ? PrColor.black : PrColor.white,
+      child: TabBar(
+        tabs: tabs,
+        isScrollable: true,
+        tabAlignment: TabAlignment.start,
+        indicatorColor: PrColor.primary,
+        unselectedLabelColor: PrColor.darkGrey,
+        labelColor: isDark ? PrColor.white : PrColor.primary,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(PrDeviceUtils.getAppBarHeight());
+}
--- pr_store/lib/features/shop/screens/store/store.dart
+++ pr_store/lib/features/shop/screens/store/store.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pr_store/common/widgets/app_bar/appbar.dart';
+import 'package:pr_store/common/widgets/app_bar/tabbar.dart';
 import 'package:pr_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:pr_store/common/widgets/layouts/grid_layout.dart';
 import 'package:pr_store/common/widgets/products/cart/cart_menu_icon.dart';
@@ -15,77 +16,89 @@ class StoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = PrHelper.isDarkMode(context);
-    return Scaffold(
-      /// AppBar
-      appBar: PrAppBar(
-        title: Text(
-          'Store',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        actions: [
-          PrCartCounterIcon(
-            iconColor: isDark ? PrColor.white : PrColor.black,
-            onPressed: () {},
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        /// AppBar
+        appBar: PrAppBar(
+          title: Text(
+            'Store',
+            style: Theme.of(context).textTheme.headlineMedium,
           ),
-        ],
-      ),
+          actions: [
+            PrCartCounterIcon(
+              iconColor: isDark ? PrColor.white : PrColor.black,
+              onPressed: () {},
+            ),
+          ],
+        ),
 
-      ///HEader
-      body: NestedScrollView(
-          headerSliverBuilder: (_, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                automaticallyImplyLeading: false,
-                pinned: true,
-                floating: true,
-                backgroundColor: Colors.transparent,
-                expandedHeight: 440,
-                flexibleSpace: Padding(
-                  padding: const EdgeInsets.all(PrSizes.defaultSpace),
-                  child: ListView(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: [
-                      ///Search Bar
-                      const SizedBox(
-                        height: PrSizes.spaceBtwItems,
-                      ),
-                      const PrSearchContainer(
-                        text: 'Search in Store',
-                        showBorder: true,
-                        showBackground: false,
-                        padding: EdgeInsets.zero,
-                      ),
-                      const SizedBox(
-                        height: PrSizes.spaceBtwSections,
-                      ),
+        ///HEader
+        body: NestedScrollView(
+            headerSliverBuilder: (_, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  pinned: true,
+                  floating: true,
+                  backgroundColor: Colors.transparent,
+                  expandedHeight: 440,
+                  flexibleSpace: Padding(
+                    padding: const EdgeInsets.all(PrSizes.defaultSpace),
+                    child: ListView(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: [
+                        ///Search Bar
+                        const SizedBox(
+                          height: PrSizes.spaceBtwItems,
+                        ),
+                        const PrSearchContainer(
+                          text: 'Search in Store',
+                          showBorder: true,
+                          showBackground: false,
+                          padding: EdgeInsets.zero,
+                        ),
+                        const SizedBox(
+                          height: PrSizes.spaceBtwSections,
+                        ),
 
-                      ///Featured Brands
-                      PrSectionHeading(
-                        title: 'Featured Brands',
-                        onPressed: () {},
-                      ),
-                      const SizedBox(
-                        height: PrSizes.spaceBtwItems / 1.5,
-                      ),
+                        ///Featured Brands
+                        PrSectionHeading(
+                          title: 'Featured Brands',
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          height: PrSizes.spaceBtwItems / 1.5,
+                        ),
 
-                      ///BRand Grid Layout
-                      PrGridLayout(
-                          itemCount: 4,
-                          mainAxisCount: 80,
-                          itemBuilder: (_, index) {
-                            //In backend part we will pass each brans and onPressed event
-                            return const PrBrandCard(
-                              showBorder: false,
-                            );
-                          }),
-                    ],
+                        ///BRand Grid Layout
+                        PrGridLayout(
+                            itemCount: 4,
+                            mainAxisCount: 80,
+                            itemBuilder: (_, index) {
+                              //In backend part we will pass each brans and onPressed event
+                              return const PrBrandCard(
+                                showBorder: false,
+                              );
+                            }),
+                      ],
+                    ),
                   ),
+
+                  ///---------------Tabs--------------------------------///
+                  bottom: const PrTabBar(tabs: [
+                    Tab(child: Text('Sports')),
+                    Tab(child: Text('Furnitures')),
+                    Tab(child: Text('Electronics')),
+                    Tab(child: Text('Clothes')),
+                    Tab(child: Text('Cosmetics')),
+                  ]),
                 ),
-              ),
-            ];
-          },
-          body: Container()),
+              ];
+            },
+            body: Container()),
+      ),
     );
   }
 }
