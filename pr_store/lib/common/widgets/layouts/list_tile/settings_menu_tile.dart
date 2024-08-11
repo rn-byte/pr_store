21c2c9132b7a0555e1399a9a5af8cf5b4f3a374c
rn-byte <ramthakur977+github@gmail.com>
2024-08-11 14:40:58 +0545

    account setting and app setting works completed

---
+++ pr_store/lib/common/widgets/layouts/list_tile/settings_menu_tile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:pr_store/utils/constants/colors.dart';
+
+class PrSettingsMenuTile extends StatelessWidget {
+  const PrSettingsMenuTile(
+      {super.key,
+      required this.icon,
+      required this.title,
+      required this.subTitle,
+      this.trailing,
+      this.onTap});
+
+  final IconData icon;
+  final String title, subTitle;
+  final Widget? trailing;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 28,
+        color: PrColor.primary,
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+      subtitle: Text(
+        subTitle,
+        style: Theme.of(context).textTheme.labelMedium,
+      ),
+      trailing: trailing,
+      onTap: onTap,
+    );
+  }
+}
--- pr_store/lib/features/personalization/screens/settings/settings.dart
+++ pr_store/lib/features/personalization/screens/settings/settings.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:pr_store/common/widgets/app_bar/appbar.dart';
 import 'package:pr_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:pr_store/common/widgets/layouts/list_tile/settings_menu_tile.dart';
+import 'package:pr_store/common/widgets/texts/section_heading.dart';
 import 'package:pr_store/utils/constants/colors.dart';
 import 'package:pr_store/utils/constants/sizes.dart';
 
@@ -18,28 +21,114 @@ class SettingScreen extends StatelessWidget {
             ///Header
 
             PrPrimaryHeaderConatiner(
-                child: Column(
-              children: [
-                ///AppBar
-                PrAppBar(
-                  title: Text('Account',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineMedium!
-                          .apply(color: PrColor.white)),
-                ),
-                const SizedBox(height: PrSizes.spaceBtwSections),
+              child: Column(
+                children: [
+                  ///AppBar
+                  PrAppBar(
+                    title: Text('Account',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium!
+                            .apply(color: PrColor.white)),
+                  ),
+                  const SizedBox(height: PrSizes.spaceBtwSections),
 
-                ///User Profile Card
-                const PrProfileUserTile(),
+                  ///User Profile Card
+                  const PrProfileUserTile(),
 
-                const SizedBox(
-                  height: PrSizes.spaceBtwSections,
-                ),
-              ],
-            ))
+                  const SizedBox(
+                    height: PrSizes.spaceBtwSections,
+                  ),
+                ],
+              ),
+            ),
 
             ///Body
+            Padding(
+              padding: const EdgeInsets.all(PrSizes.defaultSpace),
+              child: Column(
+                children: [
+                  const PrSectionHeading(
+                    title: 'Account Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: PrSizes.spaceBtwItems,
+                  ),
+                  const PrSettingsMenuTile(
+                      icon: Iconsax.safe_home,
+                      title: 'My Address ',
+                      subTitle: 'Set Shopping Delevery Address'),
+                  const PrSettingsMenuTile(
+                      icon: Iconsax.shopping_cart,
+                      title: 'My Cart ',
+                      subTitle: 'Add , remove product and move to Checkout'),
+                  const PrSettingsMenuTile(
+                      icon: Iconsax.bag_tick,
+                      title: 'My Orders ',
+                      subTitle: 'In-progress and complete orders'),
+                  const PrSettingsMenuTile(
+                      icon: Iconsax.bank,
+                      title: 'Bank Account',
+                      subTitle: 'Withdraw balance to registered bank account'),
+                  const PrSettingsMenuTile(
+                      icon: Iconsax.discount_shape,
+                      title: 'My Coupons ',
+                      subTitle: 'List of all the discounted coupons'),
+                  const PrSettingsMenuTile(
+                      icon: Iconsax.notification,
+                      title: 'Notifications',
+                      subTitle: 'Set any kind of notification message'),
+                  const PrSettingsMenuTile(
+                      icon: Iconsax.security_card,
+                      title: 'Account Privacy ',
+                      subTitle: 'Manage data usage and connected account'),
+
+                  ///App Settings
+                  const SizedBox(
+                    height: PrSizes.spaceBtwSections,
+                  ),
+                  const PrSectionHeading(
+                    title: 'App Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: PrSizes.spaceBtwItems,
+                  ),
+                  const PrSettingsMenuTile(
+                      icon: Iconsax.document_upload,
+                      title: 'Load Data',
+                      subTitle: 'Upload Data to your cloud Firebase'),
+                  PrSettingsMenuTile(
+                    icon: Iconsax.location,
+                    title: 'Geolocation',
+                    subTitle: 'Set recommendation based on location',
+                    trailing: Switch(
+                      value: true,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                  PrSettingsMenuTile(
+                    icon: Iconsax.security_user,
+                    title: 'Safe Mode',
+                    subTitle: 'Search Result is safe for all ages',
+                    trailing: Switch(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                  PrSettingsMenuTile(
+                    icon: Iconsax.image,
+                    title: 'HD Image Quality',
+                    subTitle: 'Set Image Quality to be seen',
+                    trailing: Switch(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
