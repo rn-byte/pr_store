232cc19b532f1b5a5fac6daeb5fa990b318b2791
rn-byte <ramthakur977+github@gmail.com>
2024-08-13 10:54:31 +0545

    image slider added

--- pr_store/lib/features/shop/screens/products_details/produt_detail.dart
+++ pr_store/lib/features/shop/screens/products_details/produt_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pr_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:pr_store/common/widgets/images/pr_rounded_image.dart';
 import 'package:pr_store/utils/constants/colors.dart';
 import 'package:pr_store/utils/constants/image_strings.dart';
 import 'package:pr_store/utils/constants/sizes.dart';
@@ -22,14 +23,38 @@ class PrProdutDetailScreen extends StatelessWidget {
                 child: Stack(
                   children: [
                     /// Main Large Image
-                    SizedBox(
+                    const SizedBox(
                       height: 400,
                       child: Padding(
-                        padding: const EdgeInsets.all(
-                            PrSizes.productImageRadius * 2),
+                        padding: EdgeInsets.all(PrSizes.productImageRadius * 2),
                         child: Center(
                             child: Image(
                                 image: AssetImage(PrImage.productImage8))),
+                      ),
+                    ),
+
+                    ///Image Slider
+                    Positioned(
+                      right: 0,
+                      bottom: 30,
+                      left: PrSizes.defaultSpace,
+                      child: SizedBox(
+                        height: 80,
+                        child: ListView.separated(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            physics: const AlwaysScrollableScrollPhysics(),
+                            itemBuilder: (_, index) => PrRoundedImage(
+                                width: 80,
+                                backgroundColor:
+                                    isDark ? PrColor.dark : PrColor.white,
+                                border: Border.all(color: PrColor.primary),
+                                padding: const EdgeInsets.all(PrSizes.sm),
+                                imageUrl: PrImage.productImage5),
+                            separatorBuilder: (_, __) => const SizedBox(
+                                  width: PrSizes.spaceBtwItems,
+                                ),
+                            itemCount: 6),
                       ),
                     )
                   ],
