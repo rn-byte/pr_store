527ac009ec5695de75e2ff10036ab1873d1860c3
rn-byte <ramthakur977+github@gmail.com>
2024-08-13 11:03:01 +0545

    code refractored

--- pr_store/lib/features/shop/screens/products_details/produt_detail.dart
+++ pr_store/lib/features/shop/screens/products_details/produt_detail.dart
@@ -1,66 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:pr_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import 'package:pr_store/common/widgets/images/pr_rounded_image.dart';
-import 'package:pr_store/utils/constants/colors.dart';
-import 'package:pr_store/utils/constants/image_strings.dart';
-import 'package:pr_store/utils/constants/sizes.dart';
-import 'package:pr_store/utils/helpers/helper.dart';
+
+import 'widgets/product_detail_image_slider.dart';
 
 class PrProdutDetailScreen extends StatelessWidget {
   const PrProdutDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final isDark = PrHelper.isDarkMode(context);
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             /// 1. ------- Product Image Slider-------
-            PrCurvedEdgeWidget(
-              child: Container(
-                color: isDark ? PrColor.darkGrey : PrColor.light,
-                child: Stack(
-                  children: [
-                    /// Main Large Image
-                    const SizedBox(
-                      height: 400,
-                      child: Padding(
-                        padding: EdgeInsets.all(PrSizes.productImageRadius * 2),
-                        child: Center(
-                            child: Image(
-                                image: AssetImage(PrImage.productImage8))),
-                      ),
-                    ),
-
-                    ///Image Slider
-                    Positioned(
-                      right: 0,
-                      bottom: 30,
-                      left: PrSizes.defaultSpace,
-                      child: SizedBox(
-                        height: 80,
-                        child: ListView.separated(
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            physics: const AlwaysScrollableScrollPhysics(),
-                            itemBuilder: (_, index) => PrRoundedImage(
-                                width: 80,
-                                backgroundColor:
-                                    isDark ? PrColor.dark : PrColor.white,
-                                border: Border.all(color: PrColor.primary),
-                                padding: const EdgeInsets.all(PrSizes.sm),
-                                imageUrl: PrImage.productImage5),
-                            separatorBuilder: (_, __) => const SizedBox(
-                                  width: PrSizes.spaceBtwItems,
-                                ),
-                            itemCount: 6),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
+            PrProductImageSlider()
 
             /// 2. ------- Product Details------------
           ],
---
+++ pr_store/lib/features/shop/screens/products_details/widgets/product_detail_image_slider.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:pr_store/common/widgets/app_bar/appbar.dart';
+import 'package:pr_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:pr_store/common/widgets/images/pr_rounded_image.dart';
+import 'package:pr_store/utils/constants/colors.dart';
+import 'package:pr_store/utils/constants/image_strings.dart';
+import 'package:pr_store/utils/constants/sizes.dart';
+
+import '../../../../../utils/helpers/helper.dart';
+
+class PrProductImageSlider extends StatelessWidget {
+  const PrProductImageSlider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = PrHelper.isDarkMode(context);
+    return PrCurvedEdgeWidget(
+      child: Container(
+        color: isDark ? PrColor.darkGrey : PrColor.light,
+        child: Stack(
+          children: [
+            /// Main Large Image
+            const SizedBox(
+              height: 400,
+              child: Padding(
+                padding: EdgeInsets.all(PrSizes.productImageRadius * 2),
+                child: Center(
+                    child: Image(image: AssetImage(PrImage.productImage8))),
+              ),
+            ),
+
+            ///Image Slider
+            Positioned(
+              right: 0,
+              bottom: 30,
+              left: PrSizes.defaultSpace,
+              child: SizedBox(
+                height: 80,
+                child: ListView.separated(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemBuilder: (_, index) => PrRoundedImage(
+                        width: 80,
+                        backgroundColor: isDark ? PrColor.dark : PrColor.white,
+                        border: Border.all(color: PrColor.primary),
+                        padding: const EdgeInsets.all(PrSizes.sm),
+                        imageUrl: PrImage.productImage5),
+                    separatorBuilder: (_, __) => const SizedBox(
+                          width: PrSizes.spaceBtwItems,
+                        ),
+                    itemCount: 6),
+              ),
+            ),
+            PrAppBar(
+              showBackArrow: true,
+              actions: [
+                Icon(
+                  Iconsax.heart5,
+                  color: Colors.red,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
