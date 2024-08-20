f55ddacf011eafd7e1be5ba6089fa3acbeca5f3c
rn-byte <ramthakur977+github@gmail.com>
2024-08-20 08:47:40 +0545

    new address filed added

--- pr_store/lib/features/personalization/screens/address/add_new_address.dart
+++ pr_store/lib/features/personalization/screens/address/add_new_address.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:pr_store/common/widgets/app_bar/appbar.dart';
+import 'package:pr_store/utils/constants/sizes.dart';
 
 class AddNewAddressScreen extends StatelessWidget {
   const AddNewAddressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: PrAppBar(
+        showBackArrow: true,
+        title: const Text('Add new Address'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(PrSizes.defaultSpace),
+          child: Form(
+              child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Iconsax.user), labelText: 'Name'),
+              ),
+              const SizedBox(
+                height: PrSizes.spaceBtwInputFields,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Iconsax.mobile),
+                    labelText: 'Phone Number'),
+              ),
+              const SizedBox(
+                height: PrSizes.spaceBtwInputFields,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.building_31),
+                          labelText: 'Street'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: PrSizes.spaceBtwInputFields,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.code),
+                          labelText: 'Postal Code'),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.building),
+                          labelText: 'City'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: PrSizes.spaceBtwInputFields,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.activity),
+                          labelText: 'State'),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: PrSizes.spaceBtwInputFields,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Iconsax.global), labelText: 'Country'),
+              ),
+              const SizedBox(
+                height: PrSizes.defaultSpace * 2,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child:
+                    ElevatedButton(onPressed: () {}, child: const Text('Save')),
+              )
+            ],
+          )),
+        ),
+      ),
+    );
   }
 }
