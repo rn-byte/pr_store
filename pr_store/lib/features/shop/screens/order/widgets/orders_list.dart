924c72295b90ca5c29d027cb0bea3778e5feca2e
rn-byte <ramthakur977+github@gmail.com>
2024-08-28 22:49:19 +0545

    myOrder completed

--- pr_store/lib/features/shop/screens/order/widgets/orders_list.dart
+++ pr_store/lib/features/shop/screens/order/widgets/orders_list.dart
@@ -11,38 +11,110 @@ class PrOrderListItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = PrHelper.isDarkMode(context);
-    return PrRoundedContainer(
-      showBorder: true,
-      backgroundColor: isDark ? PrColor.dark : PrColor.light,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ///Row 1
-          Row(
-            children: [
-              ///1 Icon
-              Icon(Iconsax.ship),
-              SizedBox(width: PrSizes.spaceBtwItems / 2),
-
-              /// Status and date
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Processing',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyLarge!
-                        .apply(color: PrColor.primary, fontWeightDelta: 1),
-                  ),
-                  Text('07 Nov 2024',
-                      style: Theme.of(context).textTheme.headlineSmall)
-                ],
-              )
-            ],
-          )
-        ],
+    return ListView.separated(
+      itemCount: 10,
+      separatorBuilder: (_, __) => const SizedBox(
+        height: PrSizes.spaceBtwItems,
       ),
+      itemBuilder: (_, index) {
+        return PrRoundedContainer(
+          padding: const EdgeInsets.all(PrSizes.md),
+          showBorder: true,
+          backgroundColor: isDark ? PrColor.dark : PrColor.light,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ///Row 1
+              Row(
+                children: [
+                  ///1 Icon
+                  const Icon(Iconsax.ship),
+                  const SizedBox(width: PrSizes.spaceBtwItems / 2),
+
+                  ///2 Status and date
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Processing',
+                          style: Theme.of(context).textTheme.bodyLarge!.apply(
+                              color: PrColor.primary, fontWeightDelta: 1),
+                        ),
+                        Text('07 Nov 2024',
+                            style: Theme.of(context).textTheme.headlineSmall)
+                      ],
+                    ),
+                  ),
+
+                  /// 3 Icon
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Iconsax.arrow_right_34,
+                          size: PrSizes.iconSm))
+                ],
+              ),
+              const SizedBox(height: PrSizes.spaceBtwItems),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        ///1 Icon
+                        const Icon(Iconsax.tag),
+                        const SizedBox(width: PrSizes.spaceBtwItems / 2),
+
+                        ///2 Status and date
+                        Expanded(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Order',
+                                  style:
+                                      Theme.of(context).textTheme.labelMedium),
+                              Text('[#242342]',
+                                  style:
+                                      Theme.of(context).textTheme.titleMedium)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        ///1 Icon
+                        const Icon(Iconsax.calendar),
+                        const SizedBox(width: PrSizes.spaceBtwItems / 2),
+
+                        ///2 Status and date
+                        Expanded(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Shipping Date',
+                                  style:
+                                      Theme.of(context).textTheme.labelMedium),
+                              Text('10 Nov 2024',
+                                  style:
+                                      Theme.of(context).textTheme.titleMedium)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
