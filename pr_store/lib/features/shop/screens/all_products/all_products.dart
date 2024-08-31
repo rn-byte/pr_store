1303fd83992004150af462f64cbe6dc03473d659
rn-byte <ramthakur977+github@gmail.com>
2024-08-31 16:24:54 +0545

    all product screen added

--- pr_store/lib/features/shop/screens/all_products/all_products.dart
+++ pr_store/lib/features/shop/screens/all_products/all_products.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:pr_store/common/widgets/app_bar/appbar.dart';
+import 'package:pr_store/common/widgets/layouts/grid_layout/grid_layout.dart';
+import 'package:pr_store/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:pr_store/utils/constants/sizes.dart';
 
 class AllProducts extends StatelessWidget {
@@ -10,14 +12,14 @@ class AllProducts extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:
-          PrAppBar(title: const Text('Popular Products'), showBackArrow: true),
+          const PrAppBar(title: Text('Popular Products'), showBackArrow: true),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(PrSizes.defaultSpace),
+        padding: const EdgeInsets.all(PrSizes.defaultSpace),
         child: Column(
           children: [
             /// DropDown
             DropdownButtonFormField(
-              decoration: InputDecoration(prefixIcon: Icon(Iconsax.sort)),
+              decoration: const InputDecoration(prefixIcon: Icon(Iconsax.sort)),
               onChanged: (value) {},
               items: [
                 'Name',
@@ -30,7 +32,13 @@ class AllProducts extends StatelessWidget {
                   .map((option) =>
                       DropdownMenuItem(value: option, child: Text(option)))
                   .toList(),
-            )
+            ),
+            const SizedBox(height: PrSizes.spaceBtwSections),
+
+            /// Products
+            PrGridLayout(
+                itemCount: 6,
+                itemBuilder: (_, index) => const PrProductCardVertical())
           ],
         ),
       ),
