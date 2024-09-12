52a951c2dc587305e083ef7e3c63a8c01e1efdbc
rn-byte <ramthakur977+github@gmail.com>
2024-09-12 22:26:02 +0545

    user model, userRepository,userRegistration,saving user data etc done

--- pr_store/lib/data/repositories/users/user_repository.dart
+++ pr_store/lib/data/repositories/users/user_repository.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:pr_store/features/authentication/models/user_model.dart';
@@ -14,8 +13,6 @@ class UserRepository extends GetxController {
   Future<void> saveUserRecord(UserModel user) async {
     try {
       await _db.collection("Users").doc(user.id).set(user.toJson());
-    } on FirebaseAuthException catch (e) {
-      throw PrFirebaseAuthException(e.code).message;
     } on FirebaseException catch (e) {
       throw PrFirebaseException(e.code).message;
     } on FormatException catch (_) {
