98216445ee547bb9ceba5a2ead0c0e33bf3f9ec7
rn-byte <ramthakur977+github@gmail.com>
2024-09-18 00:43:26 +0545

    fetchCategories function added

--- pr_store/lib/features/shop/controllers/category/category_controller.dart
+++ pr_store/lib/features/shop/controllers/category/category_controller.dart
@@ -1,5 +1,43 @@
 import 'package:get/get.dart';
+import 'package:pr_store/data/repositories/categories/categories_repository.dart';
+import 'package:pr_store/features/shop/models/category_model.dart';
+import 'package:pr_store/utils/popups/loaders.dart';
 
 class CategoryController extends GetxController {
   static CategoryController get instance => Get.find();
+
+  final isLoading = false.obs;
+
+  final _categoryRepository = Get.put(CategoriesRepository());
+
+  RxList<CategoryModel> allCategories = <CategoryModel>[].obs;
+  RxList<CategoryModel> featuredCategories = <CategoryModel>[].obs;
+
+  @override
+  void onInit() {
+    fetchCategories();
+    super.onInit();
+  }
+
+  /// Load category data
+  Future<void> fetchCategories() async {
+    try {
+      /// showing loader while loading categories
+      isLoading.value = true;
+
+      /// Fetch categories from data source(Firebase,api etc)
+      final categories = await _categoryRepository.getAllCategories();
+
+      /// update the categories list
+      allCategories.assignAll(categories);
+
+      /// filter featured categories  *Assigning parent category
+      featuredCategories.assignAll(allCategories.where(
+          (category) => category.isFeatured && category.parentId.isEmpty));
+    } catch (e) {
+      PrLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+    } finally {
+      /// remove loading
+    }
+  }
 }
