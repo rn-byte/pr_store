94fd7421f35eb6d1be61b09e5042696829e58281
rn-byte <ramthakur977+github@gmail.com>
2024-09-17 23:55:38 +0545

    getAllCategiries function added in categoriesRepositiry

--- pr_store/lib/data/repositories/categories/categories_repository.dart
+++ pr_store/lib/data/repositories/categories/categories_repository.dart
@@ -1,5 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:pr_store/features/shop/models/category_model.dart';
+import 'package:pr_store/utils/exceptions/firebase_exceptions.dart';
+import 'package:pr_store/utils/exceptions/platform_exceptions.dart';
 
 class CategoriesRepository extends GetxController {
   static CategoriesRepository get instance => Get.find();
+
+  /// VARIABLES
+  final _db = FirebaseFirestore.instance;
+
+  /// Get all categories
+  Future<List<CategoryModel>> getAllCategories() async {
+    try {
+      // getting all the categories
+      final snapshot = await _db.collection('categories').get();
+      final list = snapshot.docs
+          .map((document) => CategoryModel.fromSnapshot(document))
+          .toList();
+      return list;
+    } on FirebaseException catch (e) {
+      throw PrFirebaseExceptions(e.code).message;
+    } on PlatformException catch (e) {
+      throw PrPlatformExceptions(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please Try Again !';
+    }
+  }
+
+  /// Get Sub Categories
+  /// upload categories to cloud firebase
 }
