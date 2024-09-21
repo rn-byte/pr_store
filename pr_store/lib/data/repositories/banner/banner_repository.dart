226bd8ac107353515b958672ac7efa12fa099d13
rn-byte <ramthakur977+github@gmail.com>
2024-09-21 22:10:20 +0545

    Banner Repository class added

---
+++ pr_store/lib/data/repositories/banner/banner_repository.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class BannerRepository extends GetxController {
+  static BannerRepository get instance => Get.find();
+
+  /// Variables
+  final _db = FirebaseFirestore.instance;
+
+  /// Get all order related to current user
+  /// Upload banner to cloud firestore
+}
