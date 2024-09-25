b46db2b6f7ce83a051a93d491824374fa23446f5
rn-byte <ramthakur977+github@gmail.com>
2024-09-25 20:12:09 +0545

    product variations model added

--- pr_store/lib/features/shop/models/product_variations_model.dart
+++ pr_store/lib/features/shop/models/product_variations_model.dart
@@ -1,1 +1,55 @@
-class ProductVariationModel {}
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ProductVariationModel {
+  final String id;
+  String sku;
+  String image;
+  String? description;
+  double price;
+  double salePrice;
+  int stock;
+  Map<String, String> attributeValues;
+
+  ProductVariationModel({
+    required this.id,
+    this.sku = '',
+    this.image = '',
+    this.description = '',
+    this.price = 0.0,
+    this.salePrice = 0.0,
+    this.stock = 0,
+    required this.attributeValues,
+  });
+
+  /// creating empty function for clean code
+  static ProductVariationModel empty() => ProductVariationModel(id: '', attributeValues: {});
+
+  /// json Format
+  toJson() {
+    return {
+      'Id': id,
+      'SKU': sku,
+      'Image': image,
+      'Description': description,
+      'Price': price,
+      'SalePrice': salePrice,
+      'Stock': stock,
+      'AttributeValues': attributeValues,
+    };
+  }
+
+  /// Mapping json orientied document snapshot from firebase to Model
+  factory ProductVariationModel.fromJson(Map<String, dynamic> document) {
+    final data = document;
+    if (data.isEmpty) ProductVariationModel.empty();
+    return ProductVariationModel(
+      id: data['Id'],
+      sku: data['SKU'],
+      image: data['Image'],
+      description: data['Description'],
+      price: data['Price'],
+      salePrice: data['SalePrice'],
+      stock: data['Stock'],
+      attributeValues: data['AttributeValues'],
+    );
+  }
+}
