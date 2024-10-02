ce14343a7efa1ad05a55ae7871df72371f144a1b
rn-byte <ramthakur977+github@gmail.com>
2024-10-02 23:27:20 +0545

    some function added in image controller class

--- pr_store/lib/features/shop/controllers/product/images_controller.dart
+++ pr_store/lib/features/shop/controllers/product/images_controller.dart
@@ -8,7 +8,28 @@ class ImagesController extends GetxController {
   RxString selectedProductImage = ''.obs;
 
   /// Get All the image form product and Variation
-  List<String> getAllProductImages(ProductModel product) {}
+  List<String> getAllProductImages(ProductModel product) {
+    /// using set string to add unique image only
+    Set<String> images = {};
+
+    ///Load Thumbnail image
+    images.add(product.thumbnail);
+
+    /// Assiging Thumbnail image as selected image
+    selectedProductImage.value = product.thumbnail;
+
+    /// Get all the images from the product model if not null
+    if (product.images != null) {
+      images.addAll(product.images!);
+    }
+
+    /// Get all the image from the product variations if not null
+    if (product.productVariations != null || product.productVariations!.isNotEmpty) {
+      images.addAll(product.productVariations!.map((variation) => variation.image));
+    }
+
+    return images.toList();
+  }
 
   /// Show Image popup
   void showEnlargedImage(String image) {}
