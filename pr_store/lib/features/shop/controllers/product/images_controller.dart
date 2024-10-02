ee755159c026d07040b08d35c7e8ead7069fb198
rn-byte <ramthakur977+github@gmail.com>
2024-10-02 23:42:37 +0545

    image pop up function added

--- pr_store/lib/features/shop/controllers/product/images_controller.dart
+++ pr_store/lib/features/shop/controllers/product/images_controller.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pr_store/features/shop/models/product_model.dart';
+import 'package:pr_store/utils/constants/sizes.dart';
 
 class ImagesController extends GetxController {
   static ImagesController get instance => Get.find();
@@ -32,5 +35,30 @@ class ImagesController extends GetxController {
   }
 
   /// Show Image popup
-  void showEnlargedImage(String image) {}
+  void showEnlargedImage(String image) {
+    Get.to(
+        fullscreenDialog: true,
+        () => Dialog.fullscreen(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: PrSizes.defaultSpace * 2, horizontal: PrSizes.defaultSpace),
+                    child: CachedNetworkImage(imageUrl: image),
+                  ),
+                  const SizedBox(height: PrSizes.spaceBtwSections),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SizedBox(
+                      width: 150,
+                      child:
+                          OutlinedButton(onPressed: () => Get.back(), child: const Text('Close')),
+                    ),
+                  ),
+                ],
+              ),
+            ));
+  }
 }
