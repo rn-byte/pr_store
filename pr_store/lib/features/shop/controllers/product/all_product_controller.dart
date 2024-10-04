8a3eba5748151f0a8e341d6d39e2cec4ae0a3e81
rn-byte <ramthakur977+github@gmail.com>
2024-10-04 16:33:22 +0545

    future function get product by qury added

--- pr_store/lib/features/shop/controllers/product/all_product_controller.dart
+++ pr_store/lib/features/shop/controllers/product/all_product_controller.dart
@@ -1,5 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:pr_store/data/repositories/product/product_repository.dart';
+import 'package:pr_store/utils/popups/loaders.dart';
+
+import '../../models/product_model.dart';
 
 class AllProductController extends GetxController {
   static AllProductController get instance => Get.find();
+
+  final repository = ProductRepository.instance;
+
+  Future<List<ProductModel>> fetchProductsByQuery(Query? query) async {
+    try {
+      if (query == null) return [];
+
+      final product = await repository.fetchProductsByQuery(query);
+      return product;
+    } catch (e) {
+      PrLoaders.errorSnackBar(title: 'Oh Snap !', message: e.toString());
+      return [];
+    }
+  }
 }
