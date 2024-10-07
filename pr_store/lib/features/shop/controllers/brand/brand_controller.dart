b9e7a472b8f82c7fb64c4241c651c1cf3a5b0541
rn-byte <ramthakur977+github@gmail.com>
2024-10-07 18:10:13 +0545

    brand controller class added

---
+++ pr_store/lib/features/shop/controllers/brand/brand_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class BrandController extends GetxController {
+  static BrandController get instance => Get.find();
+
+  /// Load Brands
+  /// Get Brand For Category
+  /// Get Brand specific product from your data source
+}
--- pr_store/lib/features/shop/screens/all_products/all_products.dart
+++ pr_store/lib/features/shop/screens/all_products/all_products.dart
@@ -5,6 +5,7 @@ import 'package:pr_store/common/widgets/app_bar/appbar.dart';
 import 'package:pr_store/common/widgets/shimmers/vertical_product_shimmer.dart';
 import 'package:pr_store/features/shop/controllers/product/all_product_controller.dart';
 import 'package:pr_store/utils/constants/sizes.dart';
+import 'package:pr_store/utils/helpers/cloud_helper_functions.dart';
 
 import '../../../../common/widgets/products/sortable/sortable_products.dart';
 import '../../models/product_model.dart';
@@ -32,13 +33,11 @@ class AllProducts extends StatelessWidget {
             builder: (context, snapshot) {
               /// checking the state of futureBuilder Snapshot
               const loader = PrVerticalProductShimmer();
-              if (snapshot.connectionState == ConnectionState.waiting) return loader;
-              if (!snapshot.hasData || snapshot.data == null || snapshot.data!.isEmpty) {
-                return const Center(
-                  child: Text('No Data Found !'),
-                );
-              }
-              if (snapshot.hasError) return const Center(child: Text('Something Went Wrong !'));
+              final widget =
+                  PrCloudHelperFunctions.checkMultiRecordState(snapshot: snapshot, loader: loader);
+
+              /// Return appropriate widget based on snapshot data
+              if (widget != null) return widget;
 
               /// Product found
               final products = snapshot.data!;
