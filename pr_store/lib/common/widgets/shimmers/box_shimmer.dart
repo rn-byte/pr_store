1d57447b4ae46030df1c8c249b4a905a4b166b4f
rn-byte <ramthakur977+github@gmail.com>
2024-10-10 15:56:59 +0545

    boxshimmer created

--- pr_store/lib/common/widgets/shimmers/box_shimmer.dart
+++ pr_store/lib/common/widgets/shimmers/box_shimmer.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:pr_store/common/widgets/shimmers/shimmer.dart';
+import 'package:pr_store/utils/constants/sizes.dart';
 
 class PrBoxShimmer extends StatelessWidget {
   const PrBoxShimmer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        Row(
+          children: [
+            Expanded(child: PrShimmerEffect(width: 150, height: 110)),
+            SizedBox(width: PrSizes.spaceBtwItems),
+            Expanded(child: PrShimmerEffect(width: 150, height: 110)),
+            SizedBox(width: PrSizes.spaceBtwItems),
+            Expanded(child: PrShimmerEffect(width: 150, height: 110)),
+          ],
+        )
+      ],
+    );
   }
 }
