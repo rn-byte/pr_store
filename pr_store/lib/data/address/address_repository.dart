33673c1741d7a2dfc221b860cdd942d1af351a98
rn-byte <ramthakur977+github@gmail.com>
2024-10-17 09:18:17 +0545

    address model added

--- pr_store/lib/data/address/address_repository.dart
+++ pr_store/lib/data/address/address_repository.dart
@@ -1,8 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
+import '../../features/authentication/models/address_model.dart';
+import '../repositories/authentication/authentication_repository.dart';
+
 class AddressRepository extends GetxController {
   static AddressRepository get instance => Get.find();
 
   final _db = FirebaseFirestore.instance;
+
+  Future<List<AddressModel>> fetchUserAddress() async {
+    try {
+      final userId = AuthenticationRepository.instance.authUser!.uid;
+
+      if (userId.isEmpty) throw 'Unable to find user information. Try again in few minutes.';
+
+      final result = await _db.collection('Users').doc(userId).collection('Addresses').get();
+      return result.docs
+          .map((documentSnapshot) => AddressModel.fromDocumentSnapshot(documentSnapshot))
+          .toList();
+    } catch (e) {
+      throw 'Something went wrong while fetching Address Information. Try again later!';
+    }
+  }
 }
