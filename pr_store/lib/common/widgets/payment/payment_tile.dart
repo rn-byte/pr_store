1ea4d0d2bea741bfd2b16021e72e84b2b91cd6d6
rn-byte <ramthakur977+github@gmail.com>
2024-11-26 23:45:07 +0545

    paymentTile widget added

---
+++ pr_store/lib/common/widgets/payment/payment_tile.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:pr_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:pr_store/features/shop/controllers/checkout/checkout_controller.dart';
+import 'package:pr_store/features/shop/models/payment_method_model.dart';
+import 'package:pr_store/utils/constants/colors.dart';
+import 'package:pr_store/utils/constants/sizes.dart';
+import 'package:pr_store/utils/helpers/helper.dart';
+
+class PrPaymentTile extends StatelessWidget {
+  const PrPaymentTile({super.key, required this.paymentMethod});
+
+  final PaymentMethodModel paymentMethod;
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = CheckoutController.instance;
+    return ListTile(
+      contentPadding: const EdgeInsets.all(0),
+      onTap: () {
+        controller.selectedPaymentMethod.value = paymentMethod;
+        Get.back();
+      },
+      leading: PrRoundedContainer(
+        width: 60,
+        height: 40,
+        backgroundColor:
+            PrHelper.isDarkMode(context) ? PrColor.light : PrColor.dark,
+        padding: const EdgeInsets.all(PrSizes.sm),
+        child: Image(
+          image: AssetImage(paymentMethod.image),
+          fit: BoxFit.contain,
+        ),
+      ),
+      title: Text(paymentMethod.name),
+      trailing: const Icon(Iconsax.arrow_right),
+    );
+  }
+}
--- pr_store/lib/features/shop/controllers/checkout/checkout_controller.dart
+++ pr_store/lib/features/shop/controllers/checkout/checkout_controller.dart
@@ -1,5 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pr_store/common/widgets/texts/section_heading.dart';
+import 'package:pr_store/utils/constants/image_strings.dart';
+import 'package:pr_store/utils/constants/sizes.dart';
+
+import '../../../../common/widgets/payment/payment_tile.dart';
+import '../../models/payment_method_model.dart';
 
 class CheckoutController extends GetxController {
   static CheckoutController get instance => Get.find();
+
+  final Rx<PaymentMethodModel> selectedPaymentMethod =
+      PaymentMethodModel.empty().obs;
+
+  @override
+  void onInit() {
+    selectedPaymentMethod.value =
+        PaymentMethodModel(image: PrImage.khalti, name: 'Khalti');
+    super.onInit();
+  }
+
+  Future<dynamic> selectPaymentMethod(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (_) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.all(PrSizes.lg),
+                child: Column(
+                  children: [
+                    PrSectionHeading(
+                      title: 'Select Payment Method',
+                      showActionButton: false,
+                    ),
+                    SizedBox(height: PrSizes.spaceBtwSections),
+                    PrPaymentTile
+                  ],
+                ),
+              ),
+            ));
+  }
 }
